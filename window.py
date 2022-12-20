02a34d50ff2e06791d620a6e7086ad49d388e9df
Younbin LIM <105615106+YB-LIM@users.noreply.github.com>
2022-12-20 18:00:16 +0900

    Customized Tukey window function for RSGen

---
+++ window.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def tukey(M):
+    alpha=0.2;
+    n = np.arange(0, M)
+    width = int(np.floor(alpha*(M-1)/2.0))
+    n1 = n[0:width+1]
+    n2 = n[width+1:M-width-1]
+    n3 = n[M-width-1:]
+
+    w1 = 0.5 * (1 + np.cos(np.pi * (-1 + 2.0*n1/alpha/(M-1))))
+    w2 = np.ones(n2.shape)
+    w3 = 0.5 * (1 + np.cos(np.pi * (-2.0/alpha + 1 + 2.0*n3/alpha/(M-1))))
+
+    w = np.concatenate((w1, w2, w3))
+    return w
+
+
+
+
+
